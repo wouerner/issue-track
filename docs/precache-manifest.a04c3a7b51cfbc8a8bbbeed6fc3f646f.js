7dc1a4a935c177cf756e19fc89686e222471f117
wouerner <wouerner@protonmail.com>
2019-08-20 08:33:55 -0300

    deploy

---
+++ docs/precache-manifest.a04c3a7b51cfbc8a8bbbeed6fc3f646f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "645899e7192bbc5b2cc7",
+    "url": "/js/login.82b757be.js"
+  },
+  {
+    "revision": "d0af02e937bdb7cd2afb",
+    "url": "/js/listar~login.96f5cb5c.js"
+  },
+  {
+    "revision": "ba6e4d1f51bc5cddbaef",
+    "url": "/js/listar.8d6f86ec.js"
+  },
+  {
+    "revision": "b90bad3798fa611bc53d",
+    "url": "/js/chunk-vendors.7386168e.js"
+  },
+  {
+    "revision": "0b8ac85140b3ace03d23",
+    "url": "/js/app.e9565bff.js"
+  },
+  {
+    "revision": "51ede5adcba86f1bc49c54190b6b8951",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0af02e937bdb7cd2afb",
+    "url": "/css/listar~login.0c7df243.css"
+  },
+  {
+    "revision": "ba6e4d1f51bc5cddbaef",
+    "url": "/css/listar.f34c2791.css"
+  },
+  {
+    "revision": "b90bad3798fa611bc53d",
+    "url": "/css/chunk-vendors.ebd195cc.css"
+  }
+];
