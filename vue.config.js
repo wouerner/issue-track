ecf4826e9b78b9508ce89aaf17adf2f64682384f
wouerner <wouerner@protonmail.com>
2019-08-29 09:56:55 -0300

    https

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/issue-track/'
-    : '/'
+    : '/',
+  devServer: {
+    https: true,
+    allowedHosts: [
+      'issue-track.localhost',
+      'issue-track.hmg.localhost',
+    ]
+  }
 }
